beda5a01995f83156b68a0706fcbdc24043fbad4
29Niro <29niro@gmail.com>
2022-09-15 10:41:26 +0530

    test function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    <h1>Hello JS</h1>
-    <p>This is paragraph</p>
-    </>
+    <div>
+      <h1>Hello JS</h1>
+      <Welcome name="Niro"/>
+      <svg>
+        <polygon points='10 33 10 1 34 17'></polygon>
+      </svg>
+    </div>
   );
+}
+
+function Welcome(props) {
+  return <p>Hello, {props.name}</p>;
 }
 
 export default App;
