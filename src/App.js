e69b50a0057fe21208d15e260ced38b789535ec0
29Niro <29niro@gmail.com>
2022-09-15 10:45:16 +0530

    remove svg

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
     <div>
       <h1>Hello JS</h1>
       <Welcome name="Niro"/>
-      <svg>
-        <polygon points='10 33 10 1 34 17'></polygon>
-      </svg>
     </div>
   );
 }
